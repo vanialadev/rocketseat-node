17014639ee3df49859613bb5b710d85e46e9e351
Vânia Almeida <vania.almeida28@hotmail.com>
2020-06-06 18:09:33 -0300

    conectado com o banco, pegando objeto

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/nodeapi',
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+
+requireDir('./src/models/');
+
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
